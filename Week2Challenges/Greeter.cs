c64cfbe1d6d4ce35f505840f0b55561db90eb7d7
Holden Bouwers <bouwersholden@gmail.com>
2020-11-17 21:25:40 -0500

    add greeting class

---
+++ Week2Challenges/Greeter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Challenges
+{
+    public class Greeter
+    {
+        public string Name { get; set; }
+
+        public Greeter() { }
+
+        public Greeter(string name)
+        {
+            Name = name;
+        }
+        public void Hello(string name)
+        {
+            Console.WriteLine($"Hello {name}");
+        }
+
+        public void PropHello()
+        {
+            if(Name == null)
+            {
+                Console.WriteLine("What is your name?");
+                Name = Console.ReadLine();
+            }
+            Console.WriteLine($"Hello {Name}");
+        }
+
+        public void Farewell(string name)
+        {
+            Console.WriteLine($"Goodbye {name}");
+        }
+
+        public void PropFarewell()
+        {
+            if (Name == null)
+            {
+                Console.WriteLine("What is your name?");
+                Name = Console.ReadLine();
+            }
+                Console.WriteLine($"Goodbye {Name}");
+
+        }
+
+        public void Time()
+        {
+            DateTime now = DateTime.Now;
+            if(now.TimeOfDay < TimeSpan.FromHours(12))
+            {
+                Console.WriteLine("Good Morning");
+            }
+            else if(now.TimeOfDay < TimeSpan.FromHours(17))
+            {
+                Console.WriteLine("Good Afternoon");
+            }
+            else if(now.TimeOfDay < TimeSpan.FromHours(20))
+            {
+                Console.WriteLine("Good Evening");
+            }
+            else
+            {
+                Console.WriteLine("Good Night");
+            }
+        }
+    }
+}
---
+++ Week2Challenges/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Greeter greeting = new Greeter();
+            greeting.Hello("Holden");
+            greeting.Farewell("Holden");
+            greeting.PropHello();
+            greeting.PropFarewell();
+            greeting.Time();
+            Console.ReadLine();
+        }
+    }
+}
